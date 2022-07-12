99b438423018cde6906d85caf9fed4a890d0986b
Robert Wang <rwang5688@outlook.com>
2022-07-11 23:40:32 -0700

    add fake 32-character GUID

--- lib/data-edu-data-analyst-ee-stack.ts
+++ lib/data-edu-data-analyst-ee-stack.ts
@@ -3,9 +3,6 @@ import * as kms from "aws-cdk-lib/aws-kms";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as ssm from "aws-cdk-lib/aws-ssm";
-import * as eb from "aws-cdk-lib/aws-events";
-import * as targets from "aws-cdk-lib/aws-events-targets";
 import { Construct } from "constructs";
 
 export class DataEduDataAnalystEeStack extends cdk.Stack {
@@ -16,27 +13,10 @@ export class DataEduDataAnalystEeStack extends cdk.Stack {
     // Remove Default for EE Modules
     const EETeamId = new cdk.CfnParameter(this, "EETeamId", {
       type: "String",
-      default: "12_digit_aws_account_id_or_32_digit_guid",
+      default: "123456abcdefghijklmnopqrstuvwxyz",
       description: "Unique ID of this Team",
     });
     const GUID = EETeamId.valueAsString;
-
-    // DMS Role Creation CloudFormation Parameter + Condition
-    const createDMSRole = new cdk.CfnParameter(this, "createDMSRole", {
-      allowedValues: ["true", "false"],
-      constraintDescription: "Value must be set to true or false.",
-      default: "true",
-      description:
-        "Set this value to false if the 'dms-vpc-role' IAM Role has already been created in this AWS Account.",
-    });
-
-    const createDMSRoleCondition = new cdk.CfnCondition(
-      this,
-      "createDMSRoleCondition",
-      {
-        expression: cdk.Fn.conditionEquals(createDMSRole, "true"),
-      }
-    );
 
     // KMS Key Name
     const KeyName = "dataedu-key";
@@ -186,72 +166,63 @@ export class DataEduDataAnalystEeStack extends cdk.Stack {
       })
     );
 
-    // IAM Role for LMS S3 Fetch Lambda Execution Role
-    const lmsS3FetchRole = new iam.Role(this, "dataeduLMSS3FetchRole", {
+    // IAM Role for Fetch Demo Data Lambda Execution Role
+    const fetchDemoDataLambdaRole = new iam.Role(this, "dataeduFetchDemoDataLambdaRole", {
       assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-      roleName: "dataedu-fetch-s3-lambda-role",
-    });
-
-    // IAM Role for LMS API Fetch Lambda Execution Role
-    const lmsAPIFetchRole = new iam.Role(this, "dataeduLMSAPIFetchRole", {
-      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-      roleName: "dataedu-fetch-s3-data-role",
+      roleName: "dataedu-fetch-demo-data-lambda-role",
     });
 
     // Import Event Engine Asset Bucket
     const eeBucket = s3.Bucket.fromBucketName(
       this,
       "dataeduEEBucketName",
-      "ee-assets-prod-" + cdk.Stack.of(this).region
+      "ee-assets-prod-9132e5491bd44c56aaaaefc3e91b6aa8-" + cdk.Stack.of(this).region
     );
 
-    // Create LMS Config Parameter
-    const lmsSSMParam = new ssm.StringParameter(this, "dataeduSSMParam", {
-      parameterName: "/dataedu/lms-demo/state",
-      description: "SSM Parameter for mock LMS integration.",
-      stringValue:
-        '{"base_url":"ee-assets-prod-' +
-        cdk.Stack.of(this).region +
-        '.s3.amazonaws.com/modules/cfdd4f678e99415a9c1f11342a3a9887/v1/mockdata/lms_demo","version": "v1", "current_date": "2020-08-17", "perform_initial_load": "1","target_bucket":"' +
-        rawBucket.bucketName +
-        '", "base_s3_prefix":"lmsapi"}',
-    });
-
     // Create LMS S3 Fetch Lambda Function
-    const lmsS3FetchLambda = new lambda.Function(
+    const fetchDemoDataLambda = new lambda.Function(
       this,
-      "dataeduLMSS3FetchLambda",
+      "dataeduFetchDemoDataLambda",
       {
         code: lambda.Code.fromBucket(
           eeBucket,
-          "modules/cfdd4f678e99415a9c1f11342a3a9887/v1/lambda/dataedu-fetch-s3-data.zip"
+          "modules/cfdd4f678e99415a9c1f11342a3a9887/v1/lambda/data_edu_fetch_demo_data.zip"
         ),
         runtime: lambda.Runtime.PYTHON_3_7,
-        handler: "lambda_handler.lambda_handler",
-        functionName: "dataedu-fetch-s3-data",
+        handler: "data_edu_fetch_demo_data.lambda_handler.lambda_handler",
+        functionName: "data-edu-fetch-demo-data",
         memorySize: 256,
         timeout: cdk.Duration.seconds(600),
-        role: lmsS3FetchRole,
-        reservedConcurrentExecutions: 10,
+        role: fetchDemoDataLambdaRole,
+        environment: {
+          SOURCE_DATA_BUCKET_NAME_PREFIX: 'ee-assets-prod-',
+          SOURCE_MODULE_VERSION_PREFIX: 'modules/cfdd4f678e99415a9c1f11342a3a9887/v1/',
+          SIS_DEMO_MOCK_DATA_PREFIX: 'mockdata/sis_demo/',
+          LMS_DEMO_MOCK_DATA_PREFIX: 'mockdata/lms_demo/v1/',
+          RAW_DATA_BUCKET_NAME: rawBucket.bucketName,
+          SIS_DEMO_RAW_DATA_PREFIX: 'sis_demo/',
+          LMS_DEMO_RAW_DATA_PREFIX: 'lms_demo/'
+        },
         description:
-          "Lambda function that fetches a file from a URL and stores it in a S3 bucket",
+          "Lambda function that fetches demo data from source data bucket and \
+          copies the data objects to raw data bucket.",
       }
     );
 
     // Add policies to LMS S3 Fetch Lambda Execution Role
-    lmsS3FetchRole.addToPolicy(
+    fetchDemoDataLambdaRole.addToPolicy(
       new iam.PolicyStatement({
         actions: ["s3:ListBucket"],
         resources: [rawBucket.bucketArn],
       })
     );
-    lmsS3FetchRole.addToPolicy(
+    fetchDemoDataLambdaRole.addToPolicy(
       new iam.PolicyStatement({
         actions: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-        resources: [rawBucket.bucketArn + ""],
+        resources: [rawBucket.bucketArn + "/*"],
       })
     );
-    lmsS3FetchRole.addToPolicy(
+    fetchDemoDataLambdaRole.addToPolicy(
       new iam.PolicyStatement({
         actions: [
           "logs:CreateLogGroup",
@@ -263,87 +234,7 @@ export class DataEduDataAnalystEeStack extends cdk.Stack {
             cdk.Stack.of(this).region +
             ":" +
             cdk.Stack.of(this).account +
-            ":log-group:/aws/lambda/dataedu-fetch-s3-data",
-        ],
-      })
-    );
-
-    // Create LMS API Fetch Lambda Function
-    const lmsAPIFetchLambda = new lambda.Function(
-      this,
-      "dataeduLMSAPIFetchLambda",
-      {
-        code: lambda.Code.fromBucket(
-          eeBucket,
-          "modules/cfdd4f678e99415a9c1f11342a3a9887/v1/lambda/dataedu-fetch-lmsapi.zip"
-        ),
-        runtime: lambda.Runtime.PYTHON_3_7,
-        handler: "lambda_function.lambda_handler",
-        functionName: "dataedu-fetch-lmsapi",
-        memorySize: 256,
-        timeout: cdk.Duration.seconds(600),
-        role: lmsAPIFetchRole,
-        description:
-          "Lambda function that mimics invoking an API to obtain data from a SaaS app",
-        reservedConcurrentExecutions: 10,
-      }
-    );
-
-    // Create EventBridge Rule
-    const lmsAPIEventRule = new eb.Rule(this, "dataeduEventBridgeRule", {
-      description: "Invokes demo API on a scheduled basis",
-      ruleName: "dataedu-lmsapi-sync",
-      schedule: eb.Schedule.rate(cdk.Duration.minutes(1)),
-      enabled: false,
-    });
-
-    // Add Lambda Target to Event Rule
-    lmsAPIEventRule.addTarget(new targets.LambdaFunction(lmsAPIFetchLambda));
-
-    // Add policies to LMS API Fetch Lambda Execution Role
-    lmsAPIFetchRole.addToPolicy(
-      new iam.PolicyStatement({
-        actions: ["ssm:PutParameter", "ssm:GetParameter"],
-        resources: [lmsSSMParam.parameterArn],
-      })
-    );
-    lmsAPIFetchRole.addToPolicy(
-      new iam.PolicyStatement({
-        actions: ["lambda:InvokeFunction", "lambda:InvokeAsync"],
-        resources: [
-          "arn:aws:lambda:" +
-            cdk.Stack.of(this).region +
-            ":" +
-            cdk.Stack.of(this).account +
-            ":function:dataedu-*",
-        ],
-      })
-    );
-    lmsAPIFetchRole.addToPolicy(
-      new iam.PolicyStatement({
-        actions: ["events:DisableRule"],
-        resources: [
-          "arn:aws:events:" +
-            cdk.Stack.of(this).region +
-            ":" +
-            cdk.Stack.of(this).account +
-            ":rule/dataedu-lmsapi-sync",
-        ],
-      })
-    );
-    lmsAPIFetchRole.addToPolicy(
-      new iam.PolicyStatement({
-        actions: [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-        ],
-        resources: [
-          "arn:aws:logs:" +
-            cdk.Stack.of(this).region +
-            ":" +
-            cdk.Stack.of(this).account +
-            ":log-group:/aws/lambda/dataedu-fetch-lmsapi",
+            ":log-group:/aws/lambda/data-edu-fetch-demo-data",
         ],
       })
     );
